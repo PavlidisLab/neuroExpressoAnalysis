0150637f90bccd0764225aa69d9c2adb68f18304
OganM <ogan.mancarcii@gmail.com>
2016-09-12 17:26:19 -0700

    downloading part modularized

--- analysis/00.DownloadPreprocess/01.MouseCellTypeData.R
+++ analysis/00.DownloadPreprocess/01.MouseCellTypeData.R
@@ -20,31 +20,33 @@ devtools::use_data(n_expressoStudies, overwrite=TRUE)
 # non-GEO samples should be acquired through personal communication.
 # to download samples from GEO we need to extract their GSM identifiers.
 # for ease of organization we place the samples from different platforms to their own folders
+download = F
 
-GPL339 = design %>% filter(Platform == 'GPL339')
-GPL1261 = design %>% filter(Platform == 'GPL1261')
-
-dir.create('data-raw/cel/GPL339/', showWarnings = FALSE)
-GSM339 = stringr::str_extract_all(GPL339$samples,pattern='GSM[0-9]*') %>% unlist
-# download all GSMs
-GSM339 %>% sapply(function(x){
-    ogbox::gsmDown(gsm=x,
-                   outfile=paste0('data-raw/cel/GPL339/',x,'.CEL'))
-})
-
-dir.create('data-raw/cel/GPL1261/', showWarnings = FALSE)
-GPL1261 =  stringr::str_extract_all(GPL1261$samples,pattern='GSM[0-9]*') %>% unlist
-# download all GSMs
-GPL1261 %>% sapply(function(x){
-    ogbox::gsmDown(gsm=x,
-                   outfile=paste0('data-raw/cel/GPL339/',x,'.cel'))
-})
-
-# before normalization, download probe to gene annotations from gemma
-dir.create('data-raw/GemmaAnnots', showWarnings=FALSE)
-ogbox::getGemmaAnnot('GPL339','data-raw/GemmaAnnots/GPL339',annotType='noParents')
-ogbox::getGemmaAnnot('GPL1261','data-raw/GemmaAnnots/GPL1261',annotType='noParents')
-
+if (download){
+    GPL339 = design %>% filter(Platform == 'GPL339')
+    GPL1261 = design %>% filter(Platform == 'GPL1261')
+    
+    dir.create('data-raw/cel/GPL339/', showWarnings = FALSE)
+    GSM339 = stringr::str_extract_all(GPL339$samples,pattern='GSM[0-9]*') %>% unlist
+    # download all GSMs
+    GSM339 %>% sapply(function(x){
+        ogbox::gsmDown(gsm=x,
+                       outfile=paste0('data-raw/cel/GPL339/',x,'.CEL'))
+    })
+    
+    dir.create('data-raw/cel/GPL1261/', showWarnings = FALSE)
+    GPL1261 =  stringr::str_extract_all(GPL1261$samples,pattern='GSM[0-9]*') %>% unlist
+    # download all GSMs
+    GPL1261 %>% sapply(function(x){
+        ogbox::gsmDown(gsm=x,
+                       outfile=paste0('data-raw/cel/GPL339/',x,'.cel'))
+    })
+    
+    # before normalization, download probe to gene annotations from gemma
+    dir.create('data-raw/GemmaAnnots', showWarnings=FALSE)
+    ogbox::getGemmaAnnot('GPL339','data-raw/GemmaAnnots/GPL339',annotType='noParents')
+    ogbox::getGemmaAnnot('GPL1261','data-raw/GemmaAnnots/GPL1261',annotType='noParents')
+}
 
 # normalization of the data ----------------
 # here we create a file that includes all data by merging data from GPL339 and GPL1261 and one
@@ -55,19 +57,19 @@ ogbox::getGemmaAnnot('GPL1261','data-raw/GemmaAnnots/GPL1261',annotType='noParen
 # samples
 skipNorm=F
 if (skipNorm == F){
-    readDesignMergeCel(desFile='data-raw/Mouse_Cell_Type_Data/cellTypeStudies.tsv',
+    readDesignMergeCel(desFile='data-raw/Mouse_Cell_Type_Data/n_expressoStudies.tsv',
                        gsm = 'samples',
                        normalize = 'Normalize',
-                       celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=:)|(?<=[,]))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
+                       celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=[,])|(^))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
                        celDir = 'data-raw/cel', 
                        expFile = 'data-raw/Mouse_Cell_Type_Data/rmaExp.csv',
                        desOut = 'data-raw/Mouse_Cell_Type_Data/n_expressoSamples.tsv',
                        gemmaDir = "data-raw/GemmaAnnots")
     
-    readDesignMergeCel(desFile='data-raw/Mouse_Cell_Type_Data/cellTypeStudies.tsv',
+    readDesignMergeCel(desFile='data-raw/Mouse_Cell_Type_Data/n_expressoStudies.tsv',
                        gsm = 'samples',
                        normalize = 'Normalize2',
-                       celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=:)|(?<=[,]))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
+                       celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=[,])|(^))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
                        celDir = 'data-raw/cel', 
                        expFile = 'data-raw/Mouse_Cell_Type_Data/rmaExp2.csv',
                        desOut = 'data-raw/Mouse_Cell_Type_Data/n_expressoSamples2.tsv',
@@ -90,18 +92,17 @@ if (skipNorm == F){
                    design = 'data-raw/Mouse_Cell_Type_Data/n_expressoSamples2.tsv')
     
 } else if (skipNorm == T){
-    source('R/readDesignMergeCel.R')
     meltDesign(desFile='data-raw/Mouse_Cell_Type_Data/n_expressoStudies.tsv',
                gsm = 'samples',
                normalize = 'Normalize',
-               celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=:)|(?<=[,]))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
+               celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=[,])|(^))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
                exprFile = 'data-raw/Mouse_Cell_Type_Data/n_expressoExpr.csv',
                outFile = 'data-raw/Mouse_Cell_Type_Data/n_expressoSamples.tsv')
     
     meltDesign(desFile='data-raw/Mouse_Cell_Type_Data/n_expressoStudies.tsv',
                gsm = 'samples',
                normalize = 'Normalize2',
-               celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=:)|(?<=[,]))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
+               celRegex = "(GSM.*?(?=,|$))|(PC\\d....)|(Y[+].*?((?=(,))|\\d+))|((?<=[,])|(^))A((9)|(10))_[0-9]{1,}_Chee_S1_M430A|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))",
                exprFile = 'data-raw/Mouse_Cell_Type_Data/n_expressoExpr2.csv',
                outFile = 'data-raw/Mouse_Cell_Type_Data/n_expressoSamples2.tsv')
     
@@ -114,8 +115,19 @@ if (skipNorm == F){
 }
 
 # now load the data and place it in the package
-n_expressoExpr = ogbox::read.exp('data-raw//Mouse_Cell_Type_Data//finalExp.csv')
+n_expressoExpr = ogbox::read.exp('data-raw//Mouse_Cell_Type_Data//n_expressoExpr.csv')
 n_expressoSamples =  ogbox::read.design('data-raw/Mouse_Cell_Type_Data/n_expressoSamples.tsv')
 
-devtools::use_data(n_expressoExpr)
-devtools::use_data(n_expressoSamples)
+devtools::use_data(n_expressoExpr,overwrite=TRUE)
+devtools::use_data(n_expressoSamples,overwrite=TRUE)
+
+# order of cell types that makes sense
+cellOrder = n_expressoSamples %>%
+    arrange(MajorType,Neurotransmitter,PyramidalDeep) %>% 
+    filter(!is.na(PyramidalDeep)) %>% .$PyramidalDeep %>% unique
+devtools::use_data(cellOrder,overwrite=TRUE)
+
+publishableNameDictionary = n_expressoSamples %>% filter(!is.na(PyramidalDeep)) %>%  select(PyramidalDeep, ShinyNames) %>% unique
+devtools::use_data(publishableNameDictionary,overwrite=TRUE)
+
+
