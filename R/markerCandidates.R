db623dd998978c11f739f749feb1091251a0fb54
OganM <ogan.mancarcii@gmail.com>
2017-03-11 13:29:36 -0800

    markerCanditates math improvement

--- R/markerCandidates.R
+++ R/markerCandidates.R
@@ -65,9 +65,9 @@ markerCandidates = function(design,
                                      return(group2)
                                  })
         
-        g19 = groupAverage1 < 9.5 & groupAverage1 > 8
+        g19 = groupAverage1 < (log(10,base=2) + 6) & groupAverage1 > minimumExpression
         g16 = groupAverage1  < 6
-        g29 = groupAverage2 < 9.5 & groupAverage2 > 8
+        g29 = groupAverage2 < (log(10,base=2) + 6) & groupAverage2 > minimumExpression
         g26 = groupAverage2 < 6
         # this is a late addition preventing anything that is below 8 from being
         # selected. ends up removing the the differentially underexpressed stuff as well
